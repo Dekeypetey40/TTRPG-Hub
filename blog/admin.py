ac3e60d22ddc9948f076a37b9f42d20ec0b2313a
Kai <kmichaelmikhail@gmail.com>
2023-09-01 12:03:45 +0200

    Change admin to show tags

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'updated_on', 'created_on', 'status', 'get_tags']
+    
+    
+    def get_tags(self, obj):
+        return ", ".join(o for o in obj.tags.names())
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
