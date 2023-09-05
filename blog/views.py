c115e801c258dbc7481df67d81cfdcdbcb859554
Kai <kmichaelmikhail@gmail.com>
2023-09-05 12:00:00 +0200

    Add bootstrap template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Post
+from .models import Post, Comment
 
 # Create your views here.
 class PostList(generic.ListView):
