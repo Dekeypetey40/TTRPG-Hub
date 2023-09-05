902f261a68021ac1e38b084da3ef2d99cb8f9f9a
Kai <kmichaelmikhail@gmail.com>
2023-09-05 14:45:23 +0200

    Fix pagination on homepage

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
+from django.views.generic import ListView
 from django.http import HttpResponseRedirect
 from .models import Post, Comment
 
 # Create your views here.
-class PostList(generic.ListView):
+class PostList(ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
-    paginate_by = 6
+    paginate_by = 3
