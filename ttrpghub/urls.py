88ed754447cfa911598fe9583169e93d0768dc21
Kai <kmichaelmikhail@gmail.com>
2023-09-07 14:22:45 +0200

    Add link for blog posts and link urls

--- ttrpghub/urls.py
+++ ttrpghub/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("", include("blog.urls"), name="blog-urls"),
+    path('', include('blog.urls'), name='blog-urls'),
 ]
