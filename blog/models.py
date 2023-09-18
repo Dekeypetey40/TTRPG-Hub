d17379a943c69d9079dc50dab5f1edad294192a4
Kai <kmichaelmikhail@gmail.com>
2023-09-18 13:58:36 +0200

    Move models into right app

--- blog/models.py
+++ blog/models.py
@@ -42,21 +42,3 @@ class Comment(models.Model):
         
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-    
-
-class PollOption(models.Model):
-    name = models.CharField(max_length=30)
-    
-    def __str__(self):
-        return self.name
-
-
-
-class Poll(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    choices = models.ManyToManyField(
-        PollOption, related_name='related_polls', blank=True)
-    
-    def __str__(self):
-        return self.name
