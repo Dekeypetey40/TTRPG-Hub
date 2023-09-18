57f1e5a4dd12b8bfa566c26fe824d5d6efd8d75a
Kai <kmichaelmikhail@gmail.com>
2023-09-18 09:45:10 +0200

    Add tag filter view code

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from .models import Post, Comment, Tags
 from .forms import CommentForm
 
-# Create your views here.
+# View for the homepage with blog posts
 class PostList(ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -14,10 +14,17 @@ class PostList(ListView):
     posts = Post.objects.prefetch_related('tags').all()
     tags = Tag.objects.all()
 
-class TagList(ListView):
-    model = Tags
-    template_name = "index.html"
-    tags = Tag.objects.all()
+# Ability to filter blog posts by tag
+class TagFilter(View):
+    
+    def get(self, request, slug, *args, **kwargs):
+        tag = get_object_or_404(Tag, slug=slug)
+        post = Post.objects.filter(tags=tag)
+        context = {
+            'tag': tag,
+            'post': post,
+        }
+        return render(request, 'tag_filter.html', context)
     
 class PostDetail(View):
     
