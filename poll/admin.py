b5f4486d2d3d8773fb7e2a756ba3471f3f553ec9
Kai <kmichaelmikhail@gmail.com>
2023-09-19 11:33:58 +0200

    Add vote model to admin.py

--- poll/admin.py
+++ poll/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from poll.models import PollOption, Poll
+from poll.models import PollOption, Poll, Vote
 
 # Register your models here.
 
 admin.site.register(PollOption)
 admin.site.register(Poll)
+admin.site.register(Vote)
 
