58c639f18804c7531f3f8ed43f92cfc0e4d15577
Kai <kmichaelmikhail@gmail.com>
2023-09-19 10:26:05 +0200

    Continue to try to get the poll app to work

--- poll/urls.py
+++ poll/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "poll"
 
 urlpatterns = [
-    path("poll/", views.HomeView.as_view(), name="pollhome"),
+    path("poll/", views.HomeView.as_view(), name="list"),
     path("poll/<int:poll_id>/", views.PollView.as_view(), name="poll"),
 ]
