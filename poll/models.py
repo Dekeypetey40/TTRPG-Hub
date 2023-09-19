dc3ddafde19ebb80bb1e742dbbd65da77449a04f
Kai <kmichaelmikhail@gmail.com>
2023-09-19 11:32:26 +0200

    Add vote model to tally poll results

--- poll/models.py
+++ poll/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class PollOption(models.Model):
@@ -18,3 +18,17 @@ class Poll(models.Model):
     
     def __str__(self):
         return self.name
+    
+        """
+        Need a vote model so that when a poll uses the same option as
+        another that the votes get tallied properly.
+        """
+class Vote(models.Model):
+    poll = models.ForeignKey(
+        Poll, on_delete=models.SET_NULL, related_name="votes", null=True, blank=True)
+    option = models.ForeignKey(
+        PollOption, on_delete=models.SET_NULL, related_name="votes", null=True, blank=True)
+    timestamp = models.DateTimeField(default=timezone.now())
+    
+    def __str__(self):
+        return f"{self.poll.name} - {self.option.name}"
