7c7af918b22a3449a61274d0bc6db3b0e7004af3
Kai <kmichaelmikhail@gmail.com>
2023-09-22 10:47:16 +0200

    Fix typo preventing registering new accounts

--- ttrpghub/settings.py
+++ ttrpghub/settings.py
@@ -105,6 +105,12 @@ TEMPLATES = [
         },
     },
 ]
+AUTHENTICATION_BACKENDS = [
+# Needed to login by username in Django admin, regardless of `allauth`
+'django.contrib.auth.backends.ModelBackend',
+# `allauth` specific authentication methods, such as login by email
+'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 WSGI_APPLICATION = 'ttrpghub.wsgi.application'
 
@@ -129,16 +135,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa
     },
 ]
 
